89e906125ddebc2acc525e499d6bb95ecac355f9
Rafael Ferreira Alves <rafael.falves@linx.com.br>
2025-05-14 23:55:48 -0300

    Add filters.

--- actions/tasks/get-tasks.ts
+++ actions/tasks/get-tasks.ts
@@ -1,7 +1,20 @@
-import { prismadb } from "@/lib/prisma";
+import {prismadb} from "@/lib/prisma";
 
-export const getTasks = async () => {
+export const getTasks = async (status: string, priority: string, responsibleId: string) => {
+    const whereClause: any = {}
+
+    if (status) {
+        whereClause.status = status;
+    }
+    if (priority) {
+        whereClause.priority = priority;
+    }
+    if (responsibleId) {
+        whereClause.responsibleId = responsibleId
+    }
+
     return await prismadb.tasks.findMany({
+        where: whereClause,
         include: {
             responsible: true
         }
